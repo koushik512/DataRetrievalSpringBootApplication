34c523c3aa07930a998ca80abf8d315ba15d9517
Pavan Koushik <bkoushik@deloitte.com>
2022-10-18 01:20:28 +0530

    Adding Controller file and pom for JSON array changes

--- src/main/java/com/example/demo/controller/DataRetrievalController.java
+++ src/main/java/com/example/demo/controller/DataRetrievalController.java
@@ -2,12 +2,12 @@ package com.example.demo.controller;
 
 import com.example.demo.entity.Details;
 import com.example.demo.service.DataRetrievalService;
+import org.json.simple.JSONArray;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import java.sql.ResultSet;
 import java.util.List;
 
 @RestController
@@ -18,9 +18,15 @@ public class DataRetrievalController
     @Autowired
     DataRetrievalService dataRetrievalService;
 
-    @GetMapping("/getDetails")
+    @GetMapping("/getDetails/")
     public List<Details> getStocks()
     {
         return dataRetrievalService.getDetailsData();
+    }
+
+    @GetMapping("/getTableData")
+    public JSONArray getDataFromAnyTable(@RequestParam("tableName") String tableName)
+    {
+        return dataRetrievalService.retriveDataFromTable(tableName);
     }
 }
